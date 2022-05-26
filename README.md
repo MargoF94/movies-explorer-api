e3253304d0963d30d497909538b414818ee1d7dd
Margo Fedorovich <localmoyashi@yandex.ru>
2022-05-26 12:44:02 +0900

    Редактирован README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # movies-explorer-api
-Бэкенд для выпускного проекта Yandex (фронтенд)
+Бэкенд для выпускного проекта Yandex.
+
+## **Посмотреть проект:**
+
+- **IP-адрес: 51.250.13.192**
+
+- **Доступ по домену:** yourlocalmovieexplorer.nomoredomains.xyz
--- app.js
+++ app.js
@@ -52,6 +52,20 @@ app.use('*', () => {
 
 app.use(errorLogger);
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
 app.listen(PORT, () => {
   console.log("I'm working!");
 });
